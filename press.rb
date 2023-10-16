feafb51f80ac7769399e3c9c8e4ffc30e0cf922e
António Leitão <58305445+antonio-leitao@users.noreply.github.com>
2023-10-16 21:50:26 +0200

    updated press shasum

--- press.rb
+++ press.rb
@@ -2,7 +2,7 @@ class Press < Formula
   desc "A command line printing press for latex."
   homepage "https://github.com/antonio-leitao/printing-press"
   url "https://github.com/antonio-leitao/printing-press/releases/download/v0.1.0/press-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "d2bec87e6b5d1b51630b4eae73466eb99ee9c6db"
+  sha256 "34bd3a730d6aa108574ae16a09c27e1ed4539ea352665a548269dbd6cd3fb4e"
   version "0.1.0"
 
   def install
