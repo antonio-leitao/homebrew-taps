fbde1603bc14afc6d5ff0d040839436765dc2b3e
António Leitão <58305445+antonio-leitao@users.noreply.github.com>
2024-03-07 15:32:30 +0100

    Update bib.rb

--- bib.rb
+++ bib.rb
@@ -1,9 +1,9 @@
 class Bib < Formula
   desc "A command line bibliography manager."
   homepage "https://github.com/antonio-leitao/bib"
-  url "https://github.com/antonio-leitao/bib/releases/download/v0.2.2/bib.tar.gz"
-  sha256 "ac80efe05b140a589ff4ea592234bfa595966ef60a0b89c6a1989f85841f646f"
-  version "0.2.2"
+  url "https://github.com/antonio-leitao/bib/releases/download/v0.2.4/bib.tar.gz"
+  sha256 "46a5f0d2322f065e46537b70df626a7e7e04904d592b52c66be9cbb47211e28a"
+  version "0.2.4"
 
   def install
     bin.install "bib"
