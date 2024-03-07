de351af0beba4794743ea825147ee724955f200d
António Leitão <58305445+antonio-leitao@users.noreply.github.com>
2024-03-07 17:22:23 +0100

    Update bib.rb

--- bib.rb
+++ bib.rb
@@ -3,7 +3,7 @@ class Bib < Formula
   homepage "https://github.com/antonio-leitao/bib"
   url "https://github.com/antonio-leitao/bib/releases/download/v0.2.4/bib.tar.gz"
   sha256 "16f728945295069e9c66e12f273f0ad17a7d8db8c62d0d36554e6c2af1a0b57d"
-  version "0.2.4"
+  version "0.2.4b"
 
   def install
     bin.install "bib"
